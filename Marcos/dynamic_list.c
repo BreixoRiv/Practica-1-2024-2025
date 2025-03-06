f4cd49ae801bc1a45d4b9cffca3f4edf7349abab
MarcosGMonne <marcos.gonzalez.monne@udc.es>
2025-03-06 10:48:54 +0100

    6/03/2025

---
+++ Marcos/dynamic_list.c
@@ -0,0 +1,18 @@
+/*
+ * TITLE: PROGRAMMING II LABS
+ * SUBTITLE: Practical 1
+ * AUTHOR 1: ***************************** LOGIN 1: **********
+ * AUTHOR 2: ***************************** LOGIN 2: **********
+ * GROUP: *.*
+ * DATE: ** / ** / **
+ */
+
+#include "dynamic_list.h"
+
+void  createEmptyList(tList* r){
+
+  }
+
+bool isEmptyList(tList r){
+    return (r==LNULL);
+  }
