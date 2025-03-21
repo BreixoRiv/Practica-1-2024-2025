b1841a3ea691a4f23787fe422c60b8d2dcdc3e28
BreixoRiv <breixo.riveirop@udc.es>
2025-03-21 10:54:49 +0100

    21/03/2025 10:55

--- 11-02-2025/dynamic_list.h
+++ 11-02-2025/dynamic_list.h
@@ -29,8 +29,9 @@ struct tNode {
 typedef tPosL tList;
 
 //______________________________________________________________________________________________________________________
-/* Objetivo: Inicializar una lista vacía
- * Entradas: La lista a inicializar
+/* Tipo: Generadora
+ * Objetivo: Inicializar una lista vacía
+ * Entradas: La dirección de memoria de la lista a inicializar
  * Salidas: La lista inicializada
  * Postcondiciones: La lista queda inicializada y vacía
  */
@@ -38,7 +39,8 @@ typedef tPosL tList;
 void createEmptyList (tList* L);
 
 //______________________________________________________________________________________________________________________
-/* Objetivo: Comprobar si la lista está vacía
+/* Tipo: Observadora
+ * Objetivo: Comprobar si la lista está vacía
  * Entrada: La lista a comprobar
  * Salida: Devuelve TRUE si la lista está vacía y FALSE si no lo está
  * Precondición: La lista debe estar inicializada por el createEmptyList
@@ -47,7 +49,8 @@ void createEmptyList (tList* L);
 bool isEmptyList (tList L);
 
 //______________________________________________________________________________________________________________________
-/* Objetivo: Obtener la primera posición de la lista
+/* Tipo: Observadora
+ * Objetivo: Obtener la primera posición de la lista
  * Entrada: La lista de la que se obtendrá la primera posición
  * Salida: La primera posición de la lista (que siempre es 0 en una lista estática)
  * Precondiciones: La lista debe estar inicializada por el createEmptyList y no puede estar vacía
@@ -56,7 +59,8 @@ bool isEmptyList (tList L);
 tPosL first (tList L);
 
 //______________________________________________________________________________________________________________________
-/* Objetivo: Obtener la última posición de la lista
+/* Tipo: Observadora
+ * Objetivo: Obtener la última posición de la lista
  * Entrada: La lista de la que se obtendrá la última posición
  * Salida: La última posición de la lista (la cual se almacena en lastPos en una lista estática)
  * Precondiciones: La lista debe estar inicializada por el createEmptyList y no puede estar vacía
@@ -65,7 +69,8 @@ tPosL first (tList L);
 tPosL last (tList L);
 
 //______________________________________________________________________________________________________________________
-/* Objetivo: Obtener la posición siguiente a la indicada
+/* Tipo: Observadora
+ * Objetivo: Obtener la posición siguiente a la indicada
  * Entrada: La lista y la posición en la lista de la que se quiere obtener la siguiente
  * Salida: La siguiente posición a la indicada o LNULL si la posición indicada es la última
  * Precondiciones: La lista debe estar inicializada por el createEmptyList y la posición indicada tiene que ser válida
@@ -74,7 +79,8 @@ tPosL last (tList L);
 tPosL next (tPosL p, tList L);
 
 //______________________________________________________________________________________________________________________
-/* Objetivo: Obtener la posición anterior a la indicada
+/* Tipo: Observadora
+ * Objetivo: Obtener la posición anterior a la indicada
  * Entrada: La lista y la posición en la lista de la que se quiere obtener la anterior
  * Salida: La posición anterior a la indicada o LNULL si la posición indicada es la primera
  * Precondiciones: La lista debe estar inicializada por el createEmptyList y la posición indicada tiene que ser válida
@@ -83,8 +89,9 @@ tPosL next (tPosL p, tList L);
 tPosL previous (tPosL p, tList L);
 
 //______________________________________________________________________________________________________________________
-/* Objetivo : Crear un nodo
- * Entrada : La posición en la que se creará el nodo
+/* Tipo: Generadora
+ * Objetivo : Crear un nodo
+ * Entrada : La dirección de memoria de la posición en la que se creará el nodo
  * Salida : Devuelve TRUE si se ha podido crear el nodo y FALSE si no
  * Precondición : La posición debe ser válida
  */
@@ -92,8 +99,9 @@ tPosL previous (tPosL p, tList L);
 bool createNode (tPosL* p);
 
 //______________________________________________________________________________________________________________________
-/* Objetivo: Añadir un item a la lista en la posición indicada
- * Entrada: El ítem a añadir, la posición en la que se añadirá y la lista en la que se añadirá
+/* Tipo: Modificadora
+ * Objetivo: Añadir un item a la lista en la posición indicada
+ * Entrada: El ítem a añadir, la posición en la que se añadirá y la dirección de memoria de la lista en la que se añadirá
  * Salida: Devuelve TRUE junto con la lista con el ítem añadido en la posición indicada si se pudo añadir correctamente y false si no
  * Precondición: La lista debe estar inicializada por el createEmptyList y la posición indicada ser válida o nula
  * Postcondición: Las posiciones de los ítems posteriores al insertado pueden variar
@@ -102,17 +110,19 @@ bool createNode (tPosL* p);
 bool insertItem (tItemL i, tPosL p, tList *L);
 
 //______________________________________________________________________________________________________________________
-/* Objetivo: Eliminar el ítem correspondiente a la posición indicada de la lista
- * Entrada: La posición del ítem que se va a eliminar y la lista de la que se va a eliminar
+/* Tipo: Destructora
+ * Objetivo: Eliminar el ítem correspondiente a la posición indicada de la lista
+ * Entrada: La posición del ítem que se va a eliminar y la dirección de memoria de la lista de la que se va a eliminar
  * Salida: La lista con el ítem eliminado
  * Precondición: La lista debe estar inicializada por el createEmptyList y la posición indicada ser válida
- * Postcondición: Las posiciones de los ítems anteriores al eliminado pueden variar
+ * Postcondición: Las posiciones de los ítems posteriores al eliminado pueden variar
  */
 
 void deleteAtPosition (tPosL p, tList *L);
 
 //______________________________________________________________________________________________________________________
-/* Objetivo: Obtener el ítem correspondiente a una posición
+/* Tipo: Observadora
+ * Objetivo: Obtener el ítem correspondiente a una posición
  * Entrada: La posición del ítem que se quiere obtener y la lista de la que se va a obtener el ítem
  * Salida: El ítem correspondiente a la posición indicada en la lista
  * Precondición: La lista debe estar inicializada por el createEmptyList y la posición indicada ser válida
@@ -121,8 +131,9 @@ void deleteAtPosition (tPosL p, tList *L);
 tItemL getItem (tPosL p, tList L);
 
 //______________________________________________________________________________________________________________________
-/* Objetivo: Editar el contenido de un ítem
- * Entrada: Un ítem con los valores modificados, la posición del ítem que se va a editar y la lista en la que se va a editar
+/* Tipo: Modificadora
+ * Objetivo: Editar el contenido de un ítem
+ * Entrada: Un ítem con los valores modificados, la posición del ítem que se va a editar y la dirección de memoria de la lista en la que se va a editar
  * Salida: La lista con el ítem modificado
  * Precondición: La lista debe estar inicializada por el createEmptyList y la posición indicada es válida
  * Postcondición: El orden de los elementos de la lista no se puede modificar
@@ -131,7 +142,8 @@ tItemL getItem (tPosL p, tList L);
 void updateItem (tItemL i, tPosL p, tList *L);
 
 //______________________________________________________________________________________________________________________
-/* Objetivo: Obtener la posición de un ítem
+/* Tipo: Observadora
+ * Objetivo: Obtener la posición de un ítem
  * Entrada: El ítem que se quiere encontrar y la lista en la que se buscará
  * Salida: La posición del ítem buscado, en caso de que exista, o LNULL en caso de no existir
  * Precondición: La lista debe estar inicializada por el createEmptyList y la posición indicada es válida
